cbdfb5df4a0c0486a9da87e50de5a230b3b82505
liuhao <1193535804@qq.com>
2022-01-30 13:20:53 +0800

    update export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,17 +1,16 @@
-export MANPAGER="nvim +Man!"
-export PATH="$PATH:$HOME/Scripts:$HOME/.local/bin"
-export EDITOR="nvim"
-export TERMINAL="alacritty"
-export READER="zathura"
-export BROWSER="qutebrowser"
-export MANPAGER="nvim +Man!"
-
-export all_proxy=socks5://127.0.0.1:9050
-export GOPROXY=https://goproxy.io/
-
-
 export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='--bind=tab:down,shift-tab:up,ctrl-t:toggle --reverse'
+
+export MANPAGER="nvim +Man!"
+export PATH="$PATH:$HOME/.local/bin"
+export EDITOR="nvim"
+
+export TERMINAL="alacritty"
+export READER="zathura"
+export BROWSER="qutebrowser"
+
+export all_proxy=socks5://127.0.0.1:9050
+export GOPROXY=https://goproxy.io/
