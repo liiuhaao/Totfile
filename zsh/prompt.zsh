eaece7047e79a0c879549a9df983269a23ab4d8f
liuhao <1193535804@qq.com>
2022-02-07 15:07:33 +0800

    add conda

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,23 @@
 setopt transient_rprompt
 setopt prompt_subst
 
+
 add-zsh-hook precmd vcs_info
 
+
+precmd_conda_info() {
+  if [[ -n $CONDA_PREFIX ]]; then
+    CONDA_ENV="%F{#d5c4a1} $(basename $CONDA_PREFIX) %f"
+  else
+    CONDA_ENV=""
+  fi
+}
+precmd_functions+=( precmd_conda_info )
+
+
 PROMPT='%(?.%B%F{#fb4934}>%F{#fabd2f}>%F{#b8bb26}>%f%b .%B%F{#fb4934}>%F{#fb4934}>%F{#fb4934}>%f%b '
-RPROMPT='${vcs_info_msg_0_}%F{#d5c4a1}%~%f'
+RPROMPT='${CONDA_ENV}${vcs_info_msg_0_}%F{#d5c4a1}%~%f'
+
 
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '●'
