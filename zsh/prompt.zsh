6328a4a6bf8acfd0a5154e6eead16dcf339c9ca7
liuhao <1193535804@qq.com>
2024-07-31 17:01:34 +0800

    update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -14,11 +14,10 @@ precmd_conda_info() {
 }
 precmd_functions+=( precmd_conda_info )
 
-
-PROMPT='%(?.%B%F{red}>%F{yellow}>%F{green}>%f%b .%B%F{red}>%F{red}>%F{red}>%f%b '
+# PROMPT='%(?.%B%F{red}>%F{yellow}>%F{green}>%f%b .%B%F{red}>%F{red}>%F{red}>%f%b) '
 PROMPT='%B%F{red}>%F{yellow}>%F{green}>%f%b '
 RPROMPT='${CONDA_ENV}${vcs_info_msg_0_}%F{purple}%~%f %F{cyan}%m%f'
-#
+
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '●'
 zstyle ':vcs_info:*' stagedstr '●'
