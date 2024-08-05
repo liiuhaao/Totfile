14e08524748e3f89f71913a369934c3fda003f41
liuhao <1193535804@qq.com>
2024-08-05 15:07:48 +0800

    update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -17,26 +17,28 @@ add-zsh-hook precmd precmd_blank_line
 
 NEWLINE=$'\n'
 
+
 PWD='%F{cyan} %~%f'
-GIT_INFO='%F{green}${vcs_info_msg_0_}%f'
+GIT_INFO='%B%F{green}${vcs_info_msg_0_}%f%b'
 # PROMPT_CHAR='%B%F{red}>%F{yellow}>%F{green}>%f%b'
-PROMPT_CHAR='%B%F{green}󰁕%f%b'
-CONDA_ENV='%F{red} $(basename $CONDA_PREFIX)%f'
-USER='%F{blue} %n%f'
-NAME='%F{green} %m%f'
-TIME='%B%F{yellow} %*%f%b'
+PROMPT_CHAR='%B%F{green}󰁕%f%b '
+
+# NAME='%F{blue} %m%f  '
+# CONDA_ENV='%F{magenta} $(basename $CONDA_PREFIX)%f  '
+NAME='%F{blue}[%m]%f'
+CONDA_ENV='%B%F{magenta}[$(basename $CONDA_PREFIX)]%f%b'
+TIME='%B%F{yellow}[%*]%f%b'
 LINEUP=$'\e[1A'
 LINEDOWN=$'\e[1B'
 
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '%F{red}●%f'
 zstyle ':vcs_info:*' stagedstr '%F{green}●%f'
-zstyle ':vcs_info:git:*' formats '%F{green}󰊢 %b%f %F{yellow}%c%f%F{red}%u%f'
-zstyle ':vcs_info:git:*' actionformats '%F{green}󰊢 %b|%a%f %F{yellow}%c%f%F{red}%u%f'
-
+zstyle ':vcs_info:git:*' formats '%F{green}󰊢 %b%f %F{yellow}%c%f%F{red}%u%f  '
+zstyle ':vcs_info:git:*' actionformats '%F{green}󰊢 %b|%a%f %F{yellow}%c%f%F{red}%u%f  '
 
 # PROMPT="${USER} ${CONDA_ENV} ${PWD} ${GIT_INFO} ${NEWLINE}${TIME}${PROMPT_CHAR} "
 
+PROMPT="${PWD}  ${GIT_INFO}${NEWLINE}${PROMPT_CHAR}"
+RPROMPT=%{${LINEUP}%}"${CONDA_ENV} ${NAME} ${TIME}"%{${LINEDOWN}%}
 
-PROMPT="${PWD}  ${GIT_INFO}${NEWLINE}${PROMPT_CHAR} "
-RPROMPT=%{${LINEUP}%}"${CONDA_ENV}  ${USER}  ${NAME}  ${TIME}"%{${LINEDOWN}%}
