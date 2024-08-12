0db525cb17ee677c3311c6f6f879120a91302f47
liuhao <1193535804@qq.com>
2024-08-12 11:02:58 +0800

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,19 +2,18 @@ return {
     {
         -- "sainnhe/gruvbox-material",
         -- "sainnhe/everforest",
-        "neanias/everforest-nvim",
+        -- "neanias/everforest-nvim",
         -- "navarasu/onedark.nvim",
-        -- "olimorris/onedarkpro.nvim",
-        -- lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        -- priority = 1000, -- make sure to load this before all the other start plugins
+        "olimorris/onedarkpro.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            require("everforest").setup({
-                transparent_background_level = 1,
-                background = "hard",
-                italics = true,
+            require("onedarkpro").setup({
+                options = {
+                    transparency = true
+                },
             })
-            vim.cmd("colorscheme everforest")
-            -- vim.cmd("colorscheme onedark")
+            vim.cmd("colorscheme onedark")
         end,
     }
 }
