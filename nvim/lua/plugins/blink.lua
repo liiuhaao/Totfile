0588e2baaa39f1b0fb92ac12a07fb667b527590a
liuhao <1193535804@qq.com>
2025-03-08 22:51:29 +0800

    update

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -7,13 +7,19 @@ return {
     },
     version = '*',
     opts = {
-        keymap = { preset = 'default' },
-
+        keymap = {
+            preset = 'default',
+            ['<C-s>'] = { function(cmp) cmp.show() end },
+            ['<Tab>'] = { 'select_next', 'fallback' },
+            ['<S-Tab>'] = { 'select_prev', 'fallback' },
+            ['<Enter>'] = { 'select_and_accept', "fallback" },
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
         },
-
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
             providers = {
@@ -41,7 +47,6 @@ return {
                 }
             }
         },
+        signature = { enabled = true }
     },
-    opts_extend = { "sources.default" }
-
 }
