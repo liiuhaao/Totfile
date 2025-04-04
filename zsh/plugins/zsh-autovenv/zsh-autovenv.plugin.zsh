f512e2f4108081c6e604cfee648b8bc4dbc2b7a4
liuhao <1193535804@qq.com>
2025-04-04 21:35:32 +0800

    update

--- zsh/plugins/zsh-autovenv/zsh-autovenv.plugin.zsh
+++ zsh/plugins/zsh-autovenv/zsh-autovenv.plugin.zsh
@@ -5,27 +5,30 @@ CYAN="\033[0;36m"
 RESET="\033[0m"
 ICON_ENV="🔧"
 ICON_PYTHON="🐍"
+ICON_DEACTIVATE="🍂"
 function activate_venv {
     current_dir="$PWD"
     while [ "$current_dir" != "$HOME" ]; do
         if [ -d "$current_dir/.venv" ]; then
-            if [ -z "$VIRTUAL_ENV" ]; then
-                source "$current_dir/.venv/bin/activate"
-                env_name="$VIRTUAL_ENV"
-                python_version=$(python --version 2>&1)
-                echo -e "${GREEN}${ICON_ENV} Activating virtual environment: ${CYAN}$env_name${RESET}"
-                echo -e "${BLUE}${ICON_PYTHON} Python version: ${CYAN}$python_version${RESET}"
-            fi
+            source "$current_dir/.venv/bin/activate"
+            env_name="$VIRTUAL_ENV"
+            python_version=$(python --version 2>&1)
+            echo -e "${GREEN}${ICON_ENV} Activating virtual environment: ${CYAN}$env_name${RESET}"
+            echo -e "${BLUE}${ICON_PYTHON} Python version: ${CYAN}$python_version${RESET}"
             return 0
         fi
         current_dir=$(dirname "$current_dir")
     done
     if [ -n "$VIRTUAL_ENV" ]; then
-        ICON_DEACTIVATE="🍂"
         echo -e "${RED}${ICON_DEACTIVATE} Deactivating virtual environment: ${CYAN}$VIRTUAL_ENV${RESET}"
         deactivate
     fi
 }
+
+
+if [ -n "$TMUX" ]; then
+    activate_venv
+fi
 
 autoload -U add-zsh-hook
 add-zsh-hook chpwd activate_venv
