a8921808175b5e45556e642d3c9e7e2539185580
liiuhaao <1193535804@qq.com>
2025-08-14 16:38:23 +0800

    update basedpyright setting and indent-blankline

--- nvim/lua/plugins/indentmini.lua
+++
@@ -1,3 +0,0 @@
-vim.pack.add({ "https://github.com/nvimdev/indentmini.nvim" })
-
-require("indentmini").setup()
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,10 +8,43 @@ vim.pack.add({
 })
 
 require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = { "basedpyright" },
+
+local mason_lspconfig = require('mason-lspconfig');
+local lspconfig = require("lspconfig")
+local servers = {
+    basedpyright = {
+        analysis = {
+            typeCheckingMode = "basic",
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+            diagnosticMode = "openFilesOnly",
+            diagnosticSeverityOverrides = {
+                reportOptionalIterable = "warning",
+                reportAttributeAccessIssue = false,
+                reportPossiblyUnboundVariable = false,
+                reportOptionalSubscript = false,
+                reportArgumentType = false,
+            },
+            pythonPath = vim.fn.exepath("python"),
+        },
+    },
+}
+
+for server_name, settings in pairs(servers) do
+    lspconfig[server_name].setup({
+        settings = {
+            [server_name] = settings
+        },
+    })
+end
+
+mason_lspconfig.setup({
+    ensure_installed = vim.tbl_keys(servers),
+    automatic_enable = false,
     automatic_installation = true,
-})
+});
+
+
 require("mason-null-ls").setup({
     ensure_installed = { "black", "isort", "prettier" },
     automatic_installation = true,
