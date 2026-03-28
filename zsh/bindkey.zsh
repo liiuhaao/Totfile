6fab53c9580265401089b1344331ed5870df433b
liiuhaao <1193535804@qq.com>
2026-03-28 22:16:29 +0800

    update

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -1,20 +1,20 @@
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
-bindkey '^b' vi-backward-char
-bindkey '^f' vi-forward-char
-bindkey '^p' up-history
-bindkey '^n' down-history
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
-bindkey '^h' backward-delete-char
-bindkey '^d' delete-char
-bindkey '^w' backward-kill-word
-bindkey '^[f' forward-word
-bindkey '^u' kill-whole-line
-bindkey '^r' history-incremental-search-backward
-bindkey '^s' history-incremental-search-forward
-bindkey '^t' transpose-chars
+# bindkey '^b' vi-backward-char
+# bindkey '^f' vi-forward-char
+# bindkey '^p' up-history
+# bindkey '^n' down-history
+# bindkey '^a' beginning-of-line
+# bindkey '^e' end-of-line
+# bindkey '^h' backward-delete-char
+# bindkey '^d' delete-char
+# bindkey '^w' backward-kill-word
+# bindkey '^[f' forward-word
+# bindkey '^u' kill-whole-line
+# bindkey '^r' history-incremental-search-backward
+# bindkey '^s' history-incremental-search-forward
+# bindkey '^t' transpose-chars
 
-bindkey -s '^[d' 'tmux attach || tmux\n'
+# bindkey -s '^[d' 'tmux attach || tmux\n'
 # bindkey -s '^[ ' 'tmux attach || tmux\n'
 
